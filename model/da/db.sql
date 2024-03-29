e89b38bb68698b96ee29331f21f56ec081cc83d6
bahare <bahare.darbandi@gmail.com>
2024-03-29 17:17:46 +0330

    db added

--- model/da/db.sql
+++ model/da/db.sql
@@ -6,4 +6,15 @@ create table mft.person(
     family varchar(30)
 );
 
-create table lesson(12);
+create table mft.lesson(
+    id int primary key auto_increment,
+    teacher_name varchar(40),
+    name_lesson varchar(30),
+    description varchar(100)
+);
+
+create choose_course(
+    datetime_coure date ,
+    person_id int FOREIGN KEY REFERENCES mft.person (id),
+    lesson_id int FOREIGN KEY REFERENCES mft.lesson (id))
+);
