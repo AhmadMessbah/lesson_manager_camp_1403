4203a61744b486148c7dbfc1a1c7a4407fa608df
Student <khakzadmohsen32@GMAIL.COM>
2024-03-29 19:11:23 +0430

    mohsen_new

--- model/da/lesson_da.py
+++ model/da/lesson_da.py
@@ -15,37 +15,37 @@ class LessonDa:
         self.cursor.close()
         self.connection.close()
 
-    def save(self, name, teacher, discription):
+    def save(self, name_lesson, teacher_name, description):
         self.connect()
-        self.cursor.execute("INSERT INTO PERSON (name, teacher , discription) VALUES (%s,%s ,%s)",
-                            [name, teacher, discription])
+        self.cursor.execute("INSERT INTO lesson (name_lesson, teacher_name , description) VALUES (%s,%s ,%s)",
+                            [name_lesson, teacher_name, description])
         self.connection.commit()
         self.disconnect()
 
-    def edit(self, id, name, teacher,discription):
+    def edit(self, id, name_lesson, teacher_name,discription):
         self.connect()
-        self.cursor.execute("UPDATE PERSON SET NAME=%s, TECHER=%s,DISCRIPTION=%s WHERE ID=%s",
-                            [name,teacher, discription, id])
+        self.cursor.execute("UPDATE lesson SET name_lesson=%s, teacher_name=%s,description=%s WHERE ID=%s",
+                            [name_lesson,teacher_name, discription, id])
         self.connection.commit()
         self.disconnect()
 
     def remove(self, id):
         self.connect()
-        self.cursor.execute("DELETE FROM LESSON WHERE ID=%s",
+        self.cursor.execute("DELETE FROM lesson WHERE ID=%s",
                             [id])
         self.connection.commit()
         self.disconnect()
 
     def find_all(self):
         self.connect()
-        self.cursor.execute("SELECT * FROM LESSON ORDER BY TEACHER AND DISCRIPTION")
+        self.cursor.execute("SELECT * FROM lesson ORDER BY teacher_name AND description")
         lesson_list = self.cursor.fetchall()
         self.disconnect()
         return  lesson_list if  lesson_list else None
 
     def find_by_id(self, id):
         self.connect()
-        self.cursor.execute("SELECT * FROM LESSON WHERE ID=%s",
+        self.cursor.execute("SELECT * FROM lesson WHERE ID=%s",
                             [id])
         lesson = self.cursor.fetchall()
         self.disconnect()
