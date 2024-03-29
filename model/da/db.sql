dde95a625ebb1477d4d4e2c5e40ef5ddfdbeaaf9
Student <amir80man@gmail.com>
2024-03-29 18:02:17 +0430

    amir_2

--- model/da/db.sql
+++ model/da/db.sql
@@ -3,6 +3,12 @@ create database mft;
 create table mft.person(
     id int primary key auto_increment,
     name varchar(30),
-    family varchar(30)
+    family varchar(30),
+    grade varchar(50)
 );
-
+create table mft.lesson(
+    id int primary key auto_increment,
+    name varchar(30),
+    teacher varchar(30),
+    description varchar(50)
+);
