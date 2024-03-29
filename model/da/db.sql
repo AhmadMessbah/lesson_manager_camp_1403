7c671df72b521b39afe6cf347a23b5744e0c2064
bahare <bahare.darbandi@gmail.com>
2024-03-29 17:32:22 +0330

    db addedb update 3

--- model/da/db.sql
+++ model/da/db.sql
@@ -1,19 +1,19 @@
 create database mft;
---table mft.person
+-- table mft.person
 create table mft.person(
     id int primary key auto_increment,
     name varchar(30),
     family varchar(30),
-    gride varchar(30)
+    grade varchar(30)
 );
---table lesson
+-- table lesson
 create table mft.lesson(
     id int primary key auto_increment,
     teacher_name varchar(40),
     name_lesson varchar(30),
     description varchar(100)
 );
---table entkhab dars
+-- table entkhab dars
 create choose_course(
     datetime_coure date ,
     person_id int FOREIGN KEY REFERENCES mft.person (id),
