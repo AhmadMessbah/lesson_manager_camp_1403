c446ee65e2e503577bd201a0f5aa29a850c0d10b
Student <khakzadmohsen32@GMAIL.COM>
2024-03-29 20:12:30 +0430

    mohsen_new3

--- model/da/Choose_course.py
+++ model/da/Choose_course.py
@@ -14,33 +14,25 @@ class PersonDa:
         self.cursor.close()
         self.connection.close()
                                  # save person by name family grade
-    def save(self,datetime, person_id,lesson_id):
+    def save(self,datetime_coure, person_id,lesson_id):
         self.connect()
-        self.cursor.execute("INSERT INTO CHOSE_COURSE (datetime, person_id,lesson_id) VALUES (%s,%s,%s)",
-                            [datetime, person_id,lesson_id])
+        self.cursor.execute("INSERT INTO choose_course (datetime_coure, person_id,lesson_id) VALUES (%s,%s,%s)",
+                            [datetime_coure, person_id,lesson_id])
         self.connection.commit()
         self.disconnect()
 
                                       # edit person by id name family grade
-    def edit(self,datetime, person_id,lesson_id):
+    def edit(self,datetime_course, person_id,lesson_id):
         self.connect()
-        self.cursor.execute("UPDATE PERSON SET DATETIME=%s, PERSON_ID=%s ,LESSON_ID=%s WHERE ID=%s",
-                            [datetime, person_id,lesson_id])
-        self.connection.commit()
-        self.disconnect()
-
-                                       # remove person by id
-    def remove(self,id):
-        self.connect()
-        self.cursor.execute("DELETE FROM PERSON WHERE ID=%s",
-                            [id])
+        self.cursor.execute("UPDATE choose_course SET datetime_coure=%s, person_id=%s ,lesson_id=%s WHERE ID=%s",
+                            [datetime_course, person_id,lesson_id])
         self.connection.commit()
         self.disconnect()
 
                                     # find all person by family grade
     def find_all(self):
         self.connect()
-        self.cursor.execute("SELECT * FROM PERSON ORDER BY FAMILY AND grade")
+        self.cursor.execute("SELECT * FROM choose_course ORDER BY ")
         person_list = self.cursor.fetchall()
         self.disconnect()
         return person_list if person_list else None
@@ -48,7 +40,7 @@ class PersonDa:
                                       # find  person by id
     def find_by_id(self,id):
         self.connect()
-        self.cursor.execute("SELECT * FROM PERSON WHERE ID=%s",
+        self.cursor.execute("SELECT * FROM choose_course WHERE ID=%s",
                             [id])
         person = self.cursor.fetchall()
         self.disconnect()
