ba3ae30c5bc589e36534cd3a9ad6ac3dfdc207d8
bahare <bahare.darbandi@gmail.com>
2024-04-03 01:25:33 +0330

    db addedb update 4

--- controller/validator.py
+++ controller/validator.py
@@ -2,16 +2,16 @@ import re
 
 
 def name_validator(name):
-    return bool(re.match("^[a-zA-Z\s]{3,30}$", name))
+    return bool(re.match(r"^[a-zA-Z\s]{3,30}$", name))
 
 def family_validator(family):
-    return bool(re.match("^[a-zA-Z\s]{3,30}$", family))
+    return bool(re.match(r"^[a-zA-Z\s]{3,30}$", family))
 
 def grade_validator(grade):
-    return bool(re.match("^[a-zA-Z\s]{3,30}$", grade))
+    return bool(re.match(r"^[a-zA-Z\s]{3,30}$", grade))
 
 def teacher_validator(teacher):
-    return bool(re.match("^[a-zA-Z\s]{3,40}$", teacher))
+    return bool(re.match(r"^[a-zA-Z\s]{3,40}$", teacher))
 
 def description_validator(description):
-    return bool(re.match("^[a-zA-Z0-9\s]{2,100}$", description))
+    return bool(re.match(r"^[a-zA-Z0-9\s]{2,100}$", description))
